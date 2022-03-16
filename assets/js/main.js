3f7834157067001bed1368b555295b04667c10c9
Daniel Eyny <danieleyny@gmail.com>
2022-03-16 06:27:39 -0400

    under construction page

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,3 +140,49 @@ sr.reveal(`.home__social`, {delay: 600})
 sr.reveal(`.about__img, .contact__box`,{origin: 'left'})
 sr.reveal(`.about__data, .contact__form`,{origin: 'right'})
 sr.reveal(`.steps__card, .product__card, .questions__group, .footer`,{interval: 100})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Delete 
+backgroundCount = 0;
+currentFrame = 1;
+
+function restartTimer() {
+  timePrompt = setTimeout(function() {
+    changeImage();
+
+  }, 6000);
+}
+
+function changeImage() {
+  backgroundCount++;
+  imageUrl = "url('https://unsplash.it/1200/750/?random&" + backgroundCount + "')";
+
+  if (currentFrame == 1) {
+    currentName = "backgroundImage1";
+    hideName = "backgroundImage2";
+    currentFrame = 2;
+  } else {
+    currentName = "backgroundImage2";
+    hideName = "backgroundImage1";
+    currentFrame = 1;
+  }
+  document.getElementById(currentName).style.backgroundImage = imageUrl;
+  document.getElementById(hideName).className = 'hide';
+  document.getElementById(currentName).className = 'show';
+
+  restartTimer();
+}
+restartTimer();
