9aa2fc8430b02c876555f08c5b59cd865e666c79
Daniel Eyny <danieleyny@gmail.com>
2022-03-28 02:12:23 -0400

    catalog pages added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,71 @@ var data = {
 "Products" : [
 
     {
-    "ProductTitle" : "",
-    "MoreInfo" : "32 / Per Box <br/> $70/ Per Box <br/><br/> This is a test discription for the product."
+    "ProductTitle" : "Bic Plain Lighter",
+    "MoreInfo" : "50 / Pieces Per Box <br/> $50/ Per Box <br/><br/> A lighter is a portable device which generates a flame, and can be used to ignite a variety of items."
     },
-    {},
-    {}
+    {"ProductTitle" : "Bic Sports Lighter",
+    "MoreInfo" : "32 / Pierces Per Box <br/> $96/ Per Box <br/><br/> A lighter is a portable device which generates a flame, and can be used to ignite a variety of items."
+    },
+    {"ProductTitle" : "Clipper Black Lighter",
+    "MoreInfo" : "32 / Per Box <br/> $96/ Per Box <br/><br/> A lighter is a portable device which generates a flame, and can be used to ignite a variety of items."
+    }   
 ]
 
     
+}
+
+
+// console.log(data);
+
+function getData() {
+    var productList = data.Products;
+
+    productList.forEach(function(val) {
+    
+        var ProductTitle = val.ProductTitle;
+        console.log(ProductTitle);
+
+        var MoreInfo = val.MoreInfo;
+        console.log(MoreInfo);
+
+        getElementById('.grid').append();
+
+    });
 
 }
 
-console.log(data);
+
+
+getData();
+
+
+
+
+
+
+
+
+/*
+
+function getData() {
+    var productList = data.Products;
+
+    productList.forEach(function(key, val) {
+    
+        var ProductTitle = val.ProductTitle
+
+        console.log(ProductTitle);
+    });
+
+}
+
+
+
+getData();
+
+
+
+
+
+ */
